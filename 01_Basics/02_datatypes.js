12cc22006da84f2159646355d0dd52c1a6f83b45
Gulam <gulammustafa0431@gmail.com>
2026-08-16 22:11:59 +0530

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3+3) // we are using nodejs, not browser
+
+let name="mustafa"    // string
+let age=20            // number
+let isLoggedIn=false  // boolean
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standaone value
+// undefined => not assigned yet
+//symbol => unique
+
+//object
+console.log(typeof age);     // number
+console.log(typeof null);    // object
+console.log(typeof undefined);// undefined
+
+
